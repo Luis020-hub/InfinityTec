77c4c9750f36831f325bdbf989f57b155bb999f6
Luis020-hub <guhmonteiro10@hotmail.com>
2023-04-30 14:07:45 -0300

    30/04/2023
    - Finalizando o menu lateral.

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,78 +1,101 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:infinity/screens/home_screen.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({super.key});
 
-  Widget _createDrawer(IconData icon, String label) {
-    return ListTile(
-      leading: Icon(
-        icon,
-        size: 25,
-        color: Colors.black,
-      ),
-      title: Text(
-        label,
-        style: const TextStyle(
-          fontSize: 16,
-        ),
-      ),
-      onTap: () {},
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
+      child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          Container(
-            height: 120,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.bottomCenter,
-            color: Colors.blue,
-            child: const Text(
-              'Welcome to \nWorld News',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 25,
-                color: Colors.black,
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                  'https://cdn-icons-png.flaticon.com/512/219/219983.png',
+                  width: 90,
+                  height: 90,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://static.vecteezy.com/system/resources/thumbnails/001/963/715/small/paper-cut-luxury-gold-background-metal-texture-3d-abstract-vector.jpg',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            accountName: const Text('User'),
+            accountEmail: const Text('user@gmail.com'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home, color: Colors.black),
+            title: const Text('Home', style: TextStyle(fontSize: 16)),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const HomePage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications, color: Colors.black),
+            title: const Text('Notifications', style: TextStyle(fontSize: 16)),
+            onTap: () => print('Notifications'),
+            trailing: ClipOval(
+              child: Container(
+                color: Colors.red,
+                width: 23,
+                height: 23,
+                child: const Center(
+                  child: Text(
+                    '99+',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
               ),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 5,
-              vertical: 10,
-            ),
-            child: Wrap(
-              children: [
-                const SizedBox(
-                  height: 5,
-                ),
-                _createDrawer(
-                  Icons.home,
-                  'Home',
-                ),
-                _createDrawer(
-                  Icons.notifications,
-                  'Notifications',
-                ),
-                _createDrawer(
-                  Icons.star,
-                  'Favorites',
-                ),
-                _createDrawer(
-                  Icons.settings,
-                  'Settings',
-                ),
-                const Divider(color: Colors.black45),
-                _createDrawer(
-                  Icons.source,
-                  'Source',
-                ),
-              ],
-            ),
+          ListTile(
+            leading: const Icon(Icons.star, color: Colors.black),
+            title: const Text('Favorites', style: TextStyle(fontSize: 16)),
+            onTap: () => print('Favorites'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.share, color: Colors.black),
+            title: const Text('Share', style: TextStyle(fontSize: 16)),
+            onTap: () => print('Share'),
+          ),
+          const Divider(color: Colors.black26),
+          ListTile(
+            leading: const Icon(Icons.description, color: Colors.black),
+            title: const Text('Policies', style: TextStyle(fontSize: 16)),
+            onTap: () => print('Policies'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.source, color: Colors.black),
+            title: const Text('Source', style: TextStyle(fontSize: 16)),
+            onTap: () => print('Source'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings, color: Colors.black),
+            title: const Text('Settings', style: TextStyle(fontSize: 16)),
+            onTap: () => print('Settings'),
+          ),
+          const Divider(color: Colors.black26),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app, color: Colors.black),
+            title: const Text('Exit', style: TextStyle(fontSize: 16)),
+            onTap: () => print('Exit'),
           ),
         ],
       ),
